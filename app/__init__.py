68b394fd1c4c066d7c852085b02c57015e43e01e
shafin kazi <shafinkazi5@gmail.com>
2026-05-12 10:37:54 -0400

    setup

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from auth import bp as auth_bp
 import sqlite3, os
+
+app = Flask(__name__)
+app.register_blueprint(auth_bp)
+app.secret_key = "secretkey"
+DB_FILE = "koalas.db"
+
+
+@app.route("/")
+def disp_homepage():
+    session['username'] = 's'
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.debug = False
+    app.run()
