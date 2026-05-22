6557a49af92be83e89bc96e157a5592474c96387
acimpoiesu <acimpoiesu60@stuy.edu>
2026-05-21 23:36:00 -0400

    zoom + svg

--- app/static/js/prereq_graph.js
+++ app/static/js/prereq_graph.js
@@ -29,3 +29,21 @@ async function createPrereqTree() {
             children: p.map(function(val) { return { name: val }; })
         });
     });
+
+    var w = document.getElementById("graph-container").clientWidth;
+    var h = 600;
+    var rootNode = d3.hierarchy(treeData);
+    var treeLayout = d3.tree().nodeSize([15, w / 4]);
+    treeLayout(rootNode);
+
+    var svg = d3.select("#graph-container").append("svg")
+        .attr("width", "100%")
+        .attr("height", h + "px")
+        .style("font", "10px sans-serif")
+        .call(d3.zoom().on("zoom", function(e) {
+            d3.select("#main-group").attr("transform", e.transform);
+        }));
+
+    var mainG = svg.append("g")
+        .attr("id", "main-group")
+        .attr("transform", "translate(" + (w/4) + ", 15)");
